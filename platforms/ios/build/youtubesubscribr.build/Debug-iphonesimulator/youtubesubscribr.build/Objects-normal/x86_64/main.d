361226c59e4316e98597768bd5f98a92812d933a
Chaz Guerrero <chazguerrero@me.com>
2017-10-12 18:15:36 -0700

    more

---
+++ platforms/ios/build/youtubesubscribr.build/Debug-iphonesimulator/youtubesubscribr.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/chazguerrero/Documents/GitHub/ThinkLab/tns/youtube-subscribr/platforms/ios/youtubesubscribr/youtubesubscribr-Prefix.pch \
+  /Users/chazguerrero/Documents/GitHub/ThinkLab/tns/youtube-subscribr/platforms/ios/internal/main.m \
+  /Users/chazguerrero/Documents/GitHub/ThinkLab/tns/youtube-subscribr/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/chazguerrero/Documents/GitHub/ThinkLab/tns/youtube-subscribr/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/chazguerrero/Documents/GitHub/ThinkLab/tns/youtube-subscribr/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/chazguerrero/Documents/GitHub/ThinkLab/tns/youtube-subscribr/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/chazguerrero/Documents/GitHub/ThinkLab/tns/youtube-subscribr/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/chazguerrero/Documents/GitHub/ThinkLab/tns/youtube-subscribr/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/chazguerrero/Documents/GitHub/ThinkLab/tns/youtube-subscribr/platforms/ios/internal/TNSDebugging.h \
+  /Users/chazguerrero/Documents/GitHub/ThinkLab/tns/youtube-subscribr/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
